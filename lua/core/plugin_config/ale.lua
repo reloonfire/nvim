9b9aa1c784dfdebe5fabe1a6459c68e5b49150a1
Lorenzo Avallone <lorenzo.avallone01@gmail.com>
2023-01-01 22:39:45 +0100

    Added Linters Rule for Lua

--- lua/core/plugin_config/ale.lua
+++ lua/core/plugin_config/ale.lua
@@ -4,6 +4,7 @@ local let = require("vim_interfaces/vars").let
 let.g("ale_fixers", {
     go = { 'goimports', 'gofmt', 'remove_trailing_lines', 'trim_whitespace' },
     rust = { 'rustfmt', 'remove_trailing_lines', 'trim_whitespace' },
+    lua = { 'sumneko_lua', 'remove_trailing_lines', 'trim_whitespace'},
 })
 
 vim.cmd [[autocmd BufWritePre *.go,*.rs :ALEFix]]
